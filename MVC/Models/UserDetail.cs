1370d413e941db0e62f8b60e6e7491faada25bfc
Code with Shiva <shiva.1@globallogic.com>
2022-09-21 13:41:46 +0530

    CapstoneProject

--- MVC/Models/UserDetail.cs
+++ MVC/Models/UserDetail.cs
@@ -13,6 +13,7 @@ namespace MVC.Models
         public Nullable<long> ContactNo { get; set; }
         public Nullable<bool> Gender { get; set; }
         public string City { get; set; }
+
         public string Password { get; set; }
         public string Roles { get; set; }
     }
